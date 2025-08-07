ffa6ed3adce243b2e3f43687ce8e3396f8b28537
steve.kim <steve.kim@a41.io>
2025-08-07 19:27:31 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,23 @@ from dotenv import load_dotenv
 load_dotenv('config.env')
 
 class Config:
-    RPC_ENDPOINT = os.getenv('RPC_ENDPOINT', 'http://localhost:50657')
-    GRPC_ENDPOINT = os.getenv('GRPC_ENDPOINT', 'localhost:50060')
-    METRICS_ENDPOINT = os.getenv('METRICS_ENDPOINT', 'http://localhost:50660')
+    RPC_ENDPOINT = os.getenv('RPC_ENDPOINT', 'http://localhost:20657')
+    GRPC_ENDPOINT = os.getenv('GRPC_ENDPOINT', 'localhost:20060')
+    METRICS_ENDPOINT = os.getenv('METRICS_ENDPOINT', 'http://localhost:20660')
     
     EXPORTER_HOST = os.getenv('EXPORTER_HOST', '0.0.0.0')
     EXPORTER_PORT = int(os.getenv('EXPORTER_PORT', '26660'))
     
     TOKEN_DENOM = os.getenv('TOKEN_DENOM', 'u0g')
-    TOKEN_COEFFICIENT = int(os.getenv('TOKEN_COEFFICIENT', '1000000'))
+    TOKEN_COEFFICIENT = int(os.getenv('TOKEN_COEFFICIENT', '1000000000'))
     TOTAL_SUPPLY = int(os.getenv('TOTAL_SUPPLY', '1000000000'))
     
-    METRICS_SCRAPE_INTERVAL = int(os.getenv('METRICS_SCRAPE_INTERVAL', '15'))
+
+    
+    METRICS_SCRAPE_INTERVAL = int(os.getenv('METRICS_SCRAPE_INTERVAL', '10'))
     METRICS_TIMEOUT = int(os.getenv('METRICS_TIMEOUT', '10'))
     METRICS_RETRY_COUNT = int(os.getenv('METRICS_RETRY_COUNT', '3'))
     
     LOG_LEVEL = os.getenv('LOG_LEVEL', 'info')
     LOG_FORMAT = os.getenv('LOG_FORMAT', 'json')
+
